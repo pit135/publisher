f0e2e1b155e74157a0c5fe1660b30f3194127909
pit135 <wermasaubunpetrus@gmail.com>
2026-05-12 09:01:24 +0700

    Initial publisher code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut p = CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+
+    _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
+        user_id: "1".to_owned(),
+        user_name: "2406344542-Amir".to_owned()
+    });
+
+    _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
+        user_id: "2".to_owned(),
+        user_name: "2406344542-Budi".to_owned()
+    });
+
+    _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
+        user_id: "3".to_owned(),
+        user_name: "2406344542-Cica".to_owned()
+    });
+
+    _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
+        user_id: "4".to_owned(),
+        user_name: "2406344542-Dira".to_owned()
+    });
+
+    _ = p.publish_event("user_created".to_owned(), UserCreatedEventMessage {
+        user_id: "5".to_owned(),
+        user_name: "2406344542-Emir".to_owned()
+    });
 }
